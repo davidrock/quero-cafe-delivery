2d5f5b90ce231e46c31c3affe36d4dfcfaa0d6bb
David Rocha <davidrocha@outlook.com>
2022-11-13 13:00:06 +0000

    fix eslint rule to ignore empty interfaces for single extends

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -4,5 +4,5 @@ import { defaultTheme } from '../styles/themes/default';
 type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
-  export type DefaultTheme = ThemeType
+  export interface DefaultTheme extends ThemeType {}
 }
